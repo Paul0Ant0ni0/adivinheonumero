ced86559984ac32ab47973f506726ff8603f60e8
Paulo Antonio <dev.pauloantonio785@gmail.com>
2022-08-22 19:38:22 -0300

    Deploy

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@
 
 function gerar(){
     let num = document.getElementById("entrada").value;
+    //O comando Math.ceil arrendonda o número para inteiro
+    //Já o comando Math.random()*(max - min) + min) gera números randomicos.
     let num2 = Math.ceil(Math.random()*(10 - 1) + 1);
 
     if(num != "" || num != null || num != Number){
@@ -9,8 +11,9 @@ function gerar(){
             alert("Acertou")
             document.getElementById("entrada").value = ''
         }else{
-            alert("Errou")
+            alert("Errou #1F613" )
             document.getElementById("entrada").value = ''
+            
         }
        
     }else{
